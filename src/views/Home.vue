1e57c7413bdd65e19004398f67f6df75a64d115b
Aulionnn <faishalbariqmaulana123@gmail.com>
2021-05-03 10:44:58 +0700

    Add files project Shayna vue js

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Products />
+    <Insta />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Products from '@/components/Products.vue'
+import Insta from '@/components/Insta.vue'
+import Partner from '@/components/Partner.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Products,
+    Insta,
+    Partner,
+    Footer
+  }
+}
+</script>
